1b0c494afa638dc90c544e34a90cd6bbd8fb9674
Gyeongjun0917 <wjdrudwns94@gmail.com>
2022-03-08 16:54:28 +0900

    17

--- hello/quiz20.py
+++ hello/quiz20.py
@@ -1,5 +1,8 @@
 import random
+import urllib.request
+from urllib.request import urlopen
 
+from bs4 import BeautifulSoup
 
 class Quiz20:
 
@@ -20,13 +23,42 @@ class Quiz20:
         print(a2)
 
 
-    def quiz24zip(self) -> str: return None
+    def quiz24zip(self) -> str:
+
+        url = 'https://music.bugs.co.kr/chart/track/realtime/total'
+        html_doc = urlopen(url)
+        soup = BeautifulSoup(html_doc, 'lxml')
+        artists = soup.find_all('p', {'class':'artist'})
+        artists = [i.get_text() for i in artists]
+        print(''.json(i for i in artists))
+        # print(type(artists))
+        return None
+        # print(soup.find_all('p', class_="artist")[0].text)
+        '''
+        trs = soup.select("#CHARTrealtime > table > tbody > tr")
+        for tr in trs:
+            title_a = tr.select_one("th")
+            if title_a is not None:
+                print(title_a.text)
+        '''
+
+
+
+        #print(soup.prettify())
+
 
     def quiz25dictcom(self) -> str: return None
 
-    def quiz26map(self) -> str: return None
+    def quiz26(self) -> str: return None
 
-    def quiz27(self) -> str: return None
+    def quiz27melon(self) -> str:
+        herders = {'User-Agent': 'Mozilla/5.0'}
+        url = 'https://www.melon.com/chart/index.htm?dayTime=2022030816'
+        req = urllib.request.Request(url, herders=herders)
+        soup = BeautifulSoup(urlopen(req).read(), 'lxml')
+
+
+        return None
 
     def quiz28(self) -> str: return None
 
